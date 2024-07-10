322e3c1773cc1acf8f4fd77b5a1b7498437c4b15
khusan rakhmatov <khusanrakhmatov5@gmail.com>
2024-07-11 04:27:02 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+let display = document.getElementById('display');
+
+function clearDisplay() {
+    display.textContent = '';
+}
+
+function deleteLast() {
+    display.textContent = display.textContent.slice(0, -1);
+}
+
+function appendCharacter(character) {
+    display.textContent += character;
+}
+
+function calculate() {
+    try {
+        display.textContent = eval(display.textContent);
+    } catch {
+        display.textContent = 'Error';
+    }
+}
